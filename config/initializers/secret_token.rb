45834b1e4fe7ff389152d9362006ab98abf987ab
Daniel Drake <danieldrake@mesaresearch.org>
2012-12-24 14:26:39 -0800

    initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Media::Application.config.secret_token = '2a60a7e13dd0c23a9f004cbb413e9ba8f403f53f0ca5d8ce66bff39dc8e4207911137124e67574dd33766b543680979cada4616ed9721f134dcc132e26362d3d'
