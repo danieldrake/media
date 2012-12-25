241a389528a9f5a432c4284f3eb976a5f1d02d98
Daniel Drake <danieldrake@mesaresearch.org>
2012-12-24 19:28:42 -0800

    names are now displayed and linked

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -5,4 +5,20 @@ class Talk < ActiveRecord::Base
                     :length => { :minimum => 5 }
 
   has_and_belongs_to_many :presenters
+  
+  # returns an array containing presenter's names and links to their profiles
+  def peoplelinks
+  	links = Array.new
+  	presenters.each do |p|
+  		if p[:handle].length > 1 then
+  			name = "#{p[:name].split(" ")[0]} \"#{p[:handle]}\" #{p[:name].split(" ")[1]}"
+  		else
+  			name = p[:name]
+  		end
+
+  		links.push(:name => name, :link => "presenters/#{p[:id]}")
+  	end
+  	return links
+  end
+
 end
