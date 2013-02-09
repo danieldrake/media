dd00f36a5829aa8291cfd632660bf77060d6bc2f
Daniel Drake <danieldrake@mesaresearch.org>
2013-02-08 18:24:30 -0800

    bug fix

--- app/controllers/video_controller.rb
+++ app/controllers/video_controller.rb
@@ -1,18 +1,19 @@
 class VideoController < ApplicationController
   def show
-  	@video = Medium.find_by_talk_id(params[:id])
+  	video = Medium.find(:all, :conditions => { :talk_id => params[:id], :kind => "video" } )
 
-    #if @video[:kind] == "video" then
+    video = video[0] # selecting the first (and ideally ONLY) item in the array
+                     # this will cause unexpected behavior if more than one video file exists
 
-    	case @video[:mode] 
+    	case video[:mode] 
       when "wistia"
-    		@block = "<div id=\"wistia_#{@video[:data]}\" class=\"wistia_embed\" style=\"width:640px;height:480px;\" data-video-width=\"640\" data-video-height=\"480\">&nbsp;</div>
+    		@block = "<div id=\"wistia_#{video[:data]}\" class=\"wistia_embed\" style=\"width:640px;height:480px;\" data-video-width=\"640\" data-video-height=\"480\">&nbsp;</div>
   				<script charset=\"ISO-8859-1\" src=\"http://fast.wistia.com/static/concat/E-v1.js\"></script>
   				<script>
-  				wistiaEmbed = Wistia.embed(\"#{@video[:data]}\", {
+  				wistiaEmbed = Wistia.embed(\"#{video[:data]}\", {
   				  version: \"v1\",
-  				  videoWidth: #{@video[:width]},
-  				  videoHeight: #{@video[:height]},
+  				  videoWidth: #{video[:width]},
+  				  videoHeight: #{video[:height]},
   				  volumeControl: true,
   				  controlsVisibleOnLoad: true
   				});
@@ -20,16 +21,14 @@ class VideoController < ApplicationController
       #when "deeplink"
         #@block = "ugh..."
     	else
-    		@block = "Video mode #{@video[:mode]} is not yet supported. Please contact your network administrator."
+    		@block = "Video mode #{video[:mode]} is not yet supported. Please contact your network administrator."
     	end
 
-    #end
-
-    unless @block then @block = "unknown error has occured. Please contact your network administrator." end
+    unless @block then @block = "I didn't get any data from the video controller. Please contact your network administrator." end
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @video }
+      format.json { render json: video }
     end
   end
 end
